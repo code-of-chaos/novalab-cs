2e940608372e5322f7abd31a1d5f21bea0cdf3f5
AndreasSasDev <directive.athena@gmail.com>
2024-08-13 15:25:28 +0200

    Feat: Edir button works

---
+++ src/NovaLab.Client/Pages/Streams/StreamEditorForm.cs
@@ -0,0 +1,19 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using System.ComponentModel.DataAnnotations;
+
+namespace NovaLab.Client.Pages.Streams;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public class StreamEditorForm { 
+    [Required, StringLength(124, ErrorMessage = "Title Length exceeded maximum length of 124 characters")]
+    public string TwitchTitle { get; set; } = string.Empty;
+    public string? TwitchBroadcastLanguage { get; set; } = null;
+    public string? TwitchTags { get; set; } = null;
+        
+    [Required]
+    public string TwitchGameTitleName { get; set; }  = string.Empty;
+}
